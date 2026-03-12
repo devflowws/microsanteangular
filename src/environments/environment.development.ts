877e31b649b1d895ce38faf7a4a4663ddf0bdcdd
HOUNKPATI K. G. GODWAY <hounkpatigedeon4@gmail.com>
2026-03-12 00:33:30 +0000

    backend online by en

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true, // Passez à true pour la version en ligne
   firebase: {
     apiKey: "AIzaSyBjDoeAki8rmNXHynOAH5KxffBy7_5GiGw",
     authDomain: "microsanteplus.firebaseapp.com",
@@ -8,7 +8,8 @@ export const environment = {
     messagingSenderId: "926086709431",
     appId: "1:926086709431:web:4f85b23171c9e518fe0d32"
   },
-  apiUrl: "http://localhost:8181",
+  // Remplacez par votre URL Render (sans le / à la fin)
+  apiUrl: "https://microsante-backend-27d1.onrender.com",
   simulationMode: false,
   mockPayments: false
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true, // Passez à true car c'est pour la version en ligne
   firebase: {
     apiKey: "AIzaSyBjDoeAki8rmNXHynOAH5KxffBy7_5GiGw",
     authDomain: "microsanteplus.firebaseapp.com",
@@ -8,7 +8,8 @@ export const environment = {
     messagingSenderId: "926086709431",
     appId: "1:926086709431:web:4f85b23171c9e518fe0d32"
   },
-  apiUrl: "http://localhost:8181",
+  // URL de ton backend sur Render
+  apiUrl: "https://microsante-backend-27d1.onrender.com/api/v1",
   simulationMode: false,
   mockPayments: false
 };
